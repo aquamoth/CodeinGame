5a41c4b91aa2a05f1b94193c9c61f78a43072044
Mattias Åslund <mattias.aslund@mantex.se>
2015-11-12 23:41:39 +0700

    Passes fuel conservation achievment!

--- Marslander_level1/Program.cs
+++ Marslander_level1/Program.cs
@@ -15,13 +15,15 @@ class Player
 	{
 		string[] inputs;
 		int surfaceN = int.Parse(Console.ReadLine()); // the number of points used to draw the surface of Mars.
+
+		var topology = new List<Tuple<int, int>>();
 		for (int i = 0; i < surfaceN; i++)
 		{
 			inputs = Console.ReadLine().Split(' ');
 			int landX = int.Parse(inputs[0]); // X coordinate of a surface point. (0 to 6999)
 			int landY = int.Parse(inputs[1]); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+			topology.Add(new Tuple<int, int>(landX, landY));
 		}
-
 
 		const double GRAVITY = 3.711;
 		const double CRITICAL_VERTICAL_SPEED = -39.0;
@@ -39,9 +41,24 @@ class Player
 			int rotate = int.Parse(inputs[5]); // the rotation angle in degrees (-90 to 90).
 			int power = int.Parse(inputs[6]); // the thrust power (0 to 4).
 
-			if (vSpeed <= CRITICAL_VERTICAL_SPEED)
+			var lzY = topology.Where(x => x.Item1 < X).Last().Item2;
+
+			var distance = Y - lzY;
+			Console.Error.WriteLine("Distance: " + distance.ToString());
+
+			var breakingDistance = (vSpeed * vSpeed - CRITICAL_VERTICAL_SPEED * CRITICAL_VERTICAL_SPEED) / 2 / (4 - GRAVITY);
+			Console.Error.WriteLine("Breaking distance: " + breakingDistance.ToString());
+			if (breakingDistance > distance * 0.75)
 			{
 				Console.WriteLine("0 4");
+			}
+			else if (breakingDistance > distance * 0.5)
+			{
+				Console.WriteLine("0 3");
+			}
+			else if (breakingDistance > distance * 0.25)
+			{
+				Console.WriteLine("0 2");
 			}
 			else
 			{
