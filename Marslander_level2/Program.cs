0cc87159712e8eee173e7639eb00fe08916483ce
Mattias Åslund <mattias.aslund@mantex.se>
2015-11-14 02:18:12 +0700

    Not throttling until turned in the right direction. Not steering when close to ground or when all throttle is needed to break. Correctly handling horizontal breaking if overshooting LZ (sign of speed.x).

--- Marslander_level2/Program.cs
+++ Marslander_level2/Program.cs
@@ -29,7 +29,7 @@ class Point
 class Player
 {
 	const double GRAVITY = 3.711;
-	const int CRITICAL_VERTICAL_SPEED = -40;
+	const int CRITICAL_VERTICAL_SPEED = -39;
 	const int CRITICAL_HORIZONTAL_SPEED = 20;
 	const int MAX_ANGLE_KEEP_VSPEED = 21;//21.91; // acos(GRAVITY / MAX_THROTTLE)
 
@@ -67,7 +67,7 @@ class Player
 			var speed = new Point(hSpeed, vSpeed);
 
 			var waypoint = nextWaypoint(position, lz, topology);
-			var command = buildCommand(position, speed, waypoint);
+			var command = buildCommand(position, speed, rotate, waypoint);
 
 			Console.WriteLine(string.Format("{0} {1}", command.Item1, command.Item2));
 		}
@@ -93,7 +93,7 @@ class Player
 		return lz;
 	}
 
-	private static Tuple<int, int> buildCommand(Point position, Point speed, Tuple<Point, Point> waypoint)
+	private static Tuple<int, int> buildCommand(Point position, Point speed, int currentAngle, Tuple<Point, Point> waypoint)
 	{
 		//Do we need to go upwards to get to the next waypoint?
 		if (position.Y < waypoint.Item1.Y)
@@ -104,25 +104,27 @@ class Player
 		//Do we need to go horizontally to get to the next waypoint?
 		var needToGoLeft = position.X > waypoint.Item2.X;
 		if (needToGoLeft)
-			return buildCommand_GoLeft(position, speed, waypoint);
+			return buildCommand_GoLeft(position, speed, currentAngle, waypoint);
 
 		var needToGoRight = position.X < waypoint.Item1.X;
 		if (needToGoRight)
-			return buildCommand_GoRight(position, speed, waypoint);
+			return buildCommand_GoRight(position, speed, currentAngle, waypoint);
 
 
 
-		return buildCommand_VerticalLanding(position, speed, waypoint);
+		return buildCommand_VerticalLanding(position, speed, currentAngle, waypoint);
 	}
 
-	private static Tuple<int, int> buildCommand_GoRight(Point position, Point speed, Tuple<Point, Point> waypoint)
+	private static Tuple<int, int> buildCommand_GoRight(Point position, Point speed, int currentAngle, Tuple<Point, Point> waypoint)
 	{
 		//TODO: What if we are already decending? Need to stop decent first?
 		Console.Error.Write("Going to the right");
+		var throttle = currentAngle < 0 ? 0 : 4; 
 
 		var desiredFinalSpeed = CRITICAL_HORIZONTAL_SPEED; // Determine based on the next waypoint on the way
 		var possibleAcceleration = Math.Sin(Math.PI * MAX_ANGLE_KEEP_VSPEED / 180);
-		var breakingDistance = distanceFor(speed.X, desiredFinalSpeed, possibleAcceleration);
+		var breakingDistance = Math.Sign(speed.X) * distanceFor(speed.X, desiredFinalSpeed, possibleAcceleration);
+		//Console.Error.WriteLine("Breaking distance is: " + breakingDistance.ToString());
 
 		var centerOfWaypoint = (waypoint.Item1.X + waypoint.Item2.X) / 2;
 		var leftAreaOfWaypoint = (3 * waypoint.Item1.X + waypoint.Item2.X) / 4;
@@ -130,12 +132,13 @@ class Player
 		if (position.X + breakingDistance > centerOfWaypoint)
 		{
 			Console.Error.WriteLine(", breaking for center.");
-			return new Tuple<int, int>(MAX_ANGLE_KEEP_VSPEED, 4);
+			//Console.Error.WriteLine(string.Format(", breaking for center ({0}, {1}, {2}.", position, breakingDistance, centerOfWaypoint));
+			return new Tuple<int, int>(MAX_ANGLE_KEEP_VSPEED, throttle);
 		}
 		else if (position.X + breakingDistance > leftAreaOfWaypoint && speed.X>desiredFinalSpeed)
 		{
 			Console.Error.WriteLine(", breaking for desired speed.");
-			return new Tuple<int, int>(MAX_ANGLE_KEEP_VSPEED, 4);
+			return new Tuple<int, int>(MAX_ANGLE_KEEP_VSPEED, throttle);
 		}
 		else if (position.X + breakingDistance > waypoint.Item1.X)
 		{
@@ -149,33 +152,54 @@ class Player
 		}
 	}
 
-	private static Tuple<int, int> buildCommand_GoLeft(Point position, Point speed, Tuple<Point, Point> waypoint)
+	private static Tuple<int, int> buildCommand_GoLeft(Point position, Point speed, int currentAngle, Tuple<Point, Point> waypoint)
 	{
-		throw new NotImplementedException();
-		////TODO: What if we are already decending? Need to stop decent first?
-		//Console.Error.Write("Going to the left");
-		//if (speed.X < -CRITICAL_HORIZONTAL_SPEED)
-		//{
-		//	Console.Error.WriteLine(" with good speed.");
-		//	return new Tuple<int, int>(0, 4);//TODO: Should vary speed to maintain height?!
-		//}
-		//else
-		//{
-		//	Console.Error.WriteLine(", accelerating.");
-		//	return new Tuple<int, int>(MAX_ANGLE_KEEP_VSPEED, 4);
-		//}
+		//TODO: What if we are already decending? Need to stop decent first?
+		Console.Error.Write("Going to the left");
+		var throttle = currentAngle > 0 ? 0 : 4; 
+
+		var desiredFinalSpeed = -CRITICAL_HORIZONTAL_SPEED; // Determine based on the next waypoint on the way
+		var possibleAcceleration = Math.Sin(Math.PI * -MAX_ANGLE_KEEP_VSPEED / 180);
+		var breakingDistance = Math.Sign(speed.X) * distanceFor(speed.X, desiredFinalSpeed, possibleAcceleration);
+
+		var centerOfWaypoint = (waypoint.Item1.X + waypoint.Item2.X) / 2;
+		var rightAreaOfWaypoint = (waypoint.Item1.X + 3 * waypoint.Item2.X) / 4;
+
+		if (position.X + breakingDistance < centerOfWaypoint)
+		{
+			Console.Error.WriteLine(", breaking for center.");
+			return new Tuple<int, int>(-MAX_ANGLE_KEEP_VSPEED, throttle);
+		}
+		else if (position.X + breakingDistance < rightAreaOfWaypoint && speed.X < desiredFinalSpeed)
+		{
+			Console.Error.WriteLine(", breaking for desired speed.");
+			return new Tuple<int, int>(-MAX_ANGLE_KEEP_VSPEED, throttle);
+		}
+		else if (position.X + breakingDistance < waypoint.Item2.X)
+		{
+			Console.Error.WriteLine(" with good speed.");
+			return new Tuple<int, int>(0, speed.Y > 0 ? 3 : 4);
+		}
+		else
+		{
+			Console.Error.WriteLine(", accelerating.");
+			return new Tuple<int, int>(-MAX_ANGLE_KEEP_VSPEED, 4);
+		}
 	}
 
-	private static Tuple<int, int> buildCommand_VerticalLanding(Point position, Point speed, Tuple<Point, Point> waypoint)
+	private static Tuple<int, int> buildCommand_VerticalLanding(Point position, Point speed, int currentAngle, Tuple<Point, Point> waypoint)
 	{
-		var angle = 0;
+		Console.Error.WriteLine("Vertical landing");
 
-		if (speed.X > CRITICAL_HORIZONTAL_SPEED)
+		var angle = 0;
+		var centerOfWaypoint = (waypoint.Item1.X + waypoint.Item2.X) / 2;
+
+		if (speed.X > CRITICAL_HORIZONTAL_SPEED + 12 * currentAngle / 90.0 || position.X > centerOfWaypoint && speed.X > 0)
 		{
 			Console.Error.WriteLine("We are going too fast to the right");
 			angle = 90;
 		}
-		else if (speed.X < -CRITICAL_HORIZONTAL_SPEED)
+		else if (speed.X < -CRITICAL_HORIZONTAL_SPEED + 12 * currentAngle / 90.0 || position.X < centerOfWaypoint && speed.X < 0)
 		{
 			Console.Error.WriteLine("We are going too fast to the left");
 			angle = -90;
@@ -192,13 +216,19 @@ class Player
 		var breakingDistance = distanceFor(speed.Y, CRITICAL_VERTICAL_SPEED, 4 - GRAVITY);
 		Console.Error.WriteLine("Min breaking distance: " + breakingDistance.ToString());
 
-		if (breakingDistance >= distance * 0.75)
+		if (breakingDistance >= distance * 0.9 || distance < 50)
 		{
-			angle /= 6; //Max 15 degrees angle, so we get enough vertical breaking
-			return new Tuple<int, int>(angle, 4);
+			Console.Error.WriteLine("No room for horizontal control. Just take us down!");
+			return new Tuple<int, int>(0, 4);
+		}
+		else if (breakingDistance >= distance * 0.75)
+		{
+			Console.Error.WriteLine("Max break thrust with sideway control.");
+			return new Tuple<int, int>(angle / 6, 4);//Max 15 degrees angle, so we get enough vertical breaking
 		}
 		else if (angle == 0)
 		{
+			Console.Error.WriteLine("Controlled vertical descent.");
 			if (breakingDistance >= distance * 0.5)
 			{
 				return new Tuple<int, int>(0, 3);
@@ -214,11 +244,15 @@ class Player
 		}
 		else
 		{
+			Console.Error.WriteLine("Controlled descent with horizontal control.");
 			if (breakingDistance >= distance * 0.5)
 			{
-				angle /= 2;
+				return new Tuple<int, int>(angle / 2, 4);
 			}
-			return new Tuple<int, int>(angle, 4);
+			else
+			{
+				return new Tuple<int, int>(angle, 4);
+			}
 		}
 	}
 
