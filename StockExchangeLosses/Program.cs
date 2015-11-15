c2152ed5b41077ebd4bb16fee254b78bedc67b86
Mattias Åslund <mattias.aslund@mantex.se>
2015-11-15 13:48:20 +0700

    Added missing project changes. Added working Stock Exchange Losses.

---
+++ StockExchangeLosses/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+	static void Main(string[] args)
+	{
+		int n = int.Parse(Console.ReadLine());
+		string vs = Console.ReadLine();
+		Console.Error.WriteLine(vs);
+
+
+		var amounts = vs.Split(' ').Select(v => int.Parse(v)).ToArray();
+
+		var maxLoss = 0;
+		var localMax = amounts[0];
+		for (int i = 1; i < n; i++)
+		{
+			var amount = amounts[i];
+			if (amount > localMax)
+			{
+				localMax = amount;
+				Console.Error.WriteLine("New local max: " + localMax.ToString());
+			}
+			else
+			{
+				var loss = amount - localMax;
+				Console.Error.WriteLine("loss: " + loss.ToString());
+				maxLoss = Math.Min(maxLoss, loss);
+			}
+		}
+
+		Console.WriteLine(maxLoss.ToString());
+	}
+}
