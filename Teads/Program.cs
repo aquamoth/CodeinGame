1c784eeeacf8620e47fc8860134fe9cb5484938d
Mattias Åslund <mattias.aslund@mantex.se>
2015-11-17 02:11:55 +0700

    Improved init of code to try to pass speed requirements.

--- Teads/Program.cs
+++ Teads/Program.cs
@@ -17,18 +17,20 @@ class Solution
 		var sw = new Stopwatch();
 		sw.Start();
 
-		var links = new List<Tuple<string, string>>();
+		var tree = new Dictionary<string, Node>();
 		int n = int.Parse(Console.ReadLine()); // the number of adjacency relations
 		for (int i = 0; i < n; i++)
 		{
 			string[] inputs = Console.ReadLine().Split(' ');
 			var xi = inputs[0]; // the ID of a person which is adjacent to yi
 			var yi = inputs[1]; // the ID of a person which is adjacent to xi
-			links.Add(new Tuple<string, string>(xi, yi));
+			var nodex = ensureNodeInTree(tree, xi);
+			var nodey = ensureNodeInTree(tree, yi);
+			nodex.Neighbours.Add(nodey);
+			nodey.Neighbours.Add(nodex);
 		}
-		Console.Error.WriteLine("{0}\tRead input with {1} links", sw.ElapsedMilliseconds, links.Count);
+		Console.Error.WriteLine("{0}\tRead input with {1} links", sw.ElapsedMilliseconds, n);
 
-		var tree = buildTree(links);
 		Console.Error.WriteLine("{0}\tBuilt tree", sw.ElapsedMilliseconds);
 
 		var dijkstra = new Dijkstra(tree);
@@ -74,23 +76,17 @@ class Solution
 		//Console.ReadLine();
 	}
 
-	private static Dictionary<string, Node> buildTree(IEnumerable<Tuple<string, string>> links)
+	private static Node ensureNodeInTree(Dictionary<string, Node> tree, string xi)
 	{
-		var tree = links
-			.SelectMany(x => new[] { x.Item1, x.Item2 })
-			.Distinct()
-			.Select(name => new Node(name))
-			.ToDictionary(x => x.Name);
-
-		foreach (var node in tree.Values)
+		Node nodex;
+		if (tree.ContainsKey(xi))
+			nodex = tree[xi];
+		else
 		{
-			node.Neighbours = links.Where(tuple => tuple.Item1 == node.Name).Select(tuple => tuple.Item2)
-					.Union(links.Where(tuple => tuple.Item2 == node.Name).Select(tuple => tuple.Item1))
-					.Select(name => tree[name])
-					.ToArray();
+			nodex = new Node(xi);
+			tree.Add(xi, nodex);
 		}
-
-		return tree;
+		return nodex;
 	}
 
 	class Node
@@ -99,10 +95,11 @@ class Solution
 		{
 			Name = name;
 			Distance = int.MaxValue;
+			Neighbours = new List<Node>();
 		}
 
 		public string Name { get; private set; }
-		public Node[] Neighbours { get; set; }
+		public List<Node> Neighbours { get; private set; }
 		public string ShortestPath { get; set; }
 		public int Distance { get; set; }
 		public bool Visited { get; set; }
