2afcc06d77ce501aea9f6e72616b2cf4717c253e
Mattias Åslund <mattias.aslund@mantex.se>
2015-11-20 00:45:58 +0700

    Init MayanCalculator

---
+++ MayanCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+	static void Main(string[] args)
+	{
+		string[] inputs = Console.ReadLine().Split(' ');
+		int L = int.Parse(inputs[0]);
+		int H = int.Parse(inputs[1]);
+		for (int i = 0; i < H; i++)
+		{
+			string numeral = Console.ReadLine();
+		}
+		int S1 = int.Parse(Console.ReadLine());
+		for (int i = 0; i < S1; i++)
+		{
+			string num1Line = Console.ReadLine();
+		}
+		int S2 = int.Parse(Console.ReadLine());
+		for (int i = 0; i < S2; i++)
+		{
+			string num2Line = Console.ReadLine();
+		}
+		string operation = Console.ReadLine();
+
+		// Write an action using Console.WriteLine()
+		// To debug: Console.Error.WriteLine("Debug messages...");
+
+		Console.WriteLine("result");
+	}
+}
