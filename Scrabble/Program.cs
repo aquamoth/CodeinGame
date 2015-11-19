bd0abc9b31b35e7c47944b8f0d45e6c749413cd4
Mattias Åslund <mattias.aslund@mantex.se>
2015-11-20 00:32:26 +0700

    Scrabble passes all tests

--- Scrabble/Program.cs
+++ Scrabble/Program.cs
@@ -34,7 +34,50 @@ class Solution
 		{
 			get
 			{
-				return 0;//TODO
+				return this.Name.Sum(c => valueOf(c));
+			}
+		}
+
+		private int valueOf(char c)
+		{
+			switch (c)
+			{
+				case 'e':
+				case 'a':
+				case 'i':
+				case 'o':
+				case 'n':
+				case 'r':
+				case 't':
+				case 'l':
+				case 's':
+				case 'u':
+					return 1;
+				case 'd':
+				case 'g':
+					return 2;
+				case 'b':
+				case 'c':
+				case 'm':
+				case 'p':
+					return 3;
+				case 'f':
+				case 'h':
+				case 'v':
+				case 'w':
+				case 'y':
+					return 4;
+				case 'k':
+					return 5;
+				case 'j':
+				case 'x':
+					return 8;
+				case 'q':
+				case 'z':
+					return 10;
+
+				default:
+					throw new NotSupportedException("Unknown char: " + c);
 			}
 		}
 
