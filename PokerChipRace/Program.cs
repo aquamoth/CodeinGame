160828e45d2984f11ca5b907ce166b4de3c7f4fe
Mattias Åslund <mattias.aslund@mantex.se>
2015-11-26 01:49:39 +0700

    Actually winning over the default AI

--- PokerChipRace/Program.cs
+++ PokerChipRace/Program.cs
@@ -29,17 +29,49 @@ class Player
 				entities[i] = readEntityFromConsole();
 			}
 
-			var myChips = entities.Where(x=>x.Player==playerId).ToArray();
+			Console.Error.WriteLine("Entities:");
+			foreach(var e in entities)
+			{
+				Console.Error.WriteLine(e);
+			}
+
+			var expectedPositionsIn3Secs = entities.Select(x => new { Id = x.Id, Radius = x.Radius, P = x.P + x.V * 3 }).ToArray();
+			Console.Error.WriteLine("Entities are in 3 sec at:");
+			foreach (var ee in expectedPositionsIn3Secs)
+			{
+				Console.Error.WriteLine(ee.Id + ": " + ee.P);
+			}
+
+
+			var myChips = entities.Where(x => x.Player == playerId).ToArray();
 			for (int i = 0; i < playerChipCount; i++)
 			{
 				var chip = myChips[i];
+				Console.Error.WriteLine("Calculating for #" + chip.Id);
 
-				var nextTarget = entities.Where(x => x.Radius < chip.Radius).OrderByDescending(x => x.Radius).FirstOrDefault();
-				if (nextTarget!=null)
+				var expectedChipPosition = expectedPositionsIn3Secs.Where(x => x.Id == chip.Id).Single();
+				var closestSmallerChipIn3Sec = expectedPositionsIn3Secs
+					.Where(x => x.Radius < expectedChipPosition.Radius)
+					.OrderBy(x => x.P.DistanceTo(expectedChipPosition.P));
+
+				foreach (var smallerChip in closestSmallerChipIn3Sec)
 				{
-					var nextTargetIn1Sec = nextTarget.P + nextTarget.V * 1;
-					var x = Math.Round(nextTargetIn1Sec.X);
-					var y = Math.Round(nextTargetIn1Sec.Y);
+					Console.Error.WriteLine(string.Format("#{0} has range {1}", smallerChip, smallerChip.P.DistanceTo(expectedChipPosition.P)));
+				}
+
+
+				var nextTarget = closestSmallerChipIn3Sec.FirstOrDefault();//entities.Where(x => x.Radius < chip.Radius).OrderByDescending(x => x.Radius).FirstOrDefault();
+				if (nextTarget != null)
+				{
+					Console.Error.WriteLine("Closest smaller target is " + nextTarget.Id);
+					var expectedDistance = (nextTarget.P - expectedChipPosition.P);
+					Console.Error.WriteLine("Expecting distance of " + expectedDistance.Length);
+
+					var targetPosition = chip.P + expectedDistance;
+					Console.Error.WriteLine("Speeding towards " + targetPosition);
+
+					var x = Math.Round(targetPosition.X);
+					var y = Math.Round(targetPosition.Y);
 					Console.WriteLine(x + " " + y); // One instruction per chip: 2 real numbers (x y) for a propulsion, or 'WAIT'.
 				}
 			}
@@ -75,6 +107,11 @@ class Entity
 	public float Radius { get; set; }
 	public Point P { get; set; }
 	public Point V { get; set; }
+
+	public override string ToString()
+	{
+		return string.Format("#{0} ({1}) at {2} + {3} has size {4}", Id, Player, P, V, Radius);
+	}
 }
 
 class Point
@@ -90,5 +127,23 @@ class Point
 	public static Point operator +(Point p1, Point p2)
 	{
 		return new Point { X = p1.X + p2.X, Y = p1.Y + p2.Y };
+	}
+
+	public static Point operator -(Point p1, Point p2)
+	{
+		return new Point { X = p1.X - p2.X, Y = p1.Y - p2.Y };
+	}
+
+	public float Length { get { return (float)Math.Sqrt(X * X + Y * Y); } }
+
+	public float DistanceTo(Point p)
+	{
+		var vector = this - p;
+		return vector.Length;
+	}
+
+	public override string ToString()
+	{
+		return string.Format("({0}, {1})", X, Y);
 	}
 }
