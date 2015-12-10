a3dbeb766daca61a75489f2134b79b0b8af24098
Mattias Åslund <mattias.aslund@mantex.se>
2015-12-10 22:53:32 +0700

    Passing 5 tests, but working with commands that can be better measured

--- ParanoidAndroid L2/Program.cs
+++ ParanoidAndroid L2/Program.cs
@@ -38,7 +38,7 @@ class Player
 
 		var elevatorsLookup = elevators.ToLookup(x=>x.Item1, x=>x.Item2);
 
-		int[] bestPath = null;
+		Queue<Command> bestPath = null;
 
 		// game loop
 		while (true)
@@ -55,93 +55,176 @@ class Player
 			{
 				Console.Error.WriteLine("Calculating paths");
 
-				var paths = pathsFrom(0, clonePos, exitFloor, exitPos, elevatorsLookup, nbAdditionalElevators).ToArray();
-				var pathsWithDistances = paths.Select(path =>
-				{
-					var distance = Math.Abs(Math.Abs(path[0]) - clonePos);
-					for (var i = 1; i < path.Length; i++)
-					{
-						distance += Math.Abs(Math.Abs(path[i]) - Math.Abs(path[i - 1]));
-					}
-					return new
-					{
-						Path = path,
-						Distance = distance
-					};
-				}).ToArray();
+				//, 
+				var paths = pathsFrom(0, clonePos, direction == "RIGHT", exitFloor, exitPos, elevatorsLookup, nbAdditionalElevators).ToArray();
+
+	
+				//var pathsWithDistances = paths.Select(path =>
+				//{
+				//	var distance = Math.Abs(Math.Abs(path[0]) - clonePos);
+				//	for (var i = 1; i < path.Length; i++)
+				//	{
+				//		distance += Math.Abs(Math.Abs(path[i]) - Math.Abs(path[i - 1]));
+				//	}
+				//	return new
+				//	{
+				//		Path = path,
+				//		Distance = distance
+				//	};
+				//}).ToArray();
 				Console.Error.WriteLine("Considering following paths:");
-				foreach (var path in pathsWithDistances.OrderBy(x => x.Distance))
+				//foreach (var path in pathsWithDistances.OrderBy(x => x.Distance))
+				foreach (var path in paths)
 				{
-					Console.Error.WriteLine("Distance {0}:\t{1}", path.Distance, string.Join(", ", path.Path));
+					Console.Error.WriteLine(string.Join(", ", path.Select(x => x.ToString()).ToArray()));
 				}
 
-				bestPath = pathsWithDistances.OrderBy(x => x.Distance).First().Path;
+				//bestPath = pathsWithDistances.OrderBy(x => x.Distance).First().Path;
+				bestPath = new Queue<Command>(paths.First());
 			}
 
 
 			if (cloneFloor == -1)
 			{
 				Console.Error.WriteLine("No clone to control right now");
-				Console.WriteLine("WAIT"); // action: WAIT or BLOCK
+				Console.WriteLine("WAIT");
+			}
+			else if (!bestPath.Any())
+			{
+				Console.Error.WriteLine("No more commands are required");
+				Console.WriteLine("WAIT");
 			}
 			else
 			{
-				var targetPos = bestPath[cloneFloor];
-				if (clonePos == -targetPos)
+				var nextCommand = bestPath.Peek();
+				if (cloneFloor != nextCommand.Floor)
 				{
-					Console.Error.WriteLine("Creating elevator at {0}", clonePos);
-					Console.WriteLine("ELEVATOR");
-					bestPath[cloneFloor] = -bestPath[cloneFloor];
+					Console.Error.WriteLine("Waiting to elevate to floor " + nextCommand.Floor);
+					Console.WriteLine("WAIT");
 				}
-				else if (clonePos == targetPos)
+				else if (clonePos != nextCommand.Position)
 				{
-					Console.Error.WriteLine("Reached elevator at {0}", clonePos);
-					Console.WriteLine("WAIT"); // action: WAIT or BLOCK
-				}
-				else if ((direction == "RIGHT") ^ (clonePos > Math.Abs(targetPos)))
-				{
-					Console.Error.WriteLine("Traveling to {0}", targetPos);
-					Console.WriteLine("WAIT"); // action: WAIT or BLOCK
+					Console.Error.WriteLine("Waiting to reach position " + nextCommand.Position);
+					Console.WriteLine("WAIT");
 				}
 				else
 				{
-					Console.Error.WriteLine("Blocking at {0} so clones can reach {1}", clonePos, targetPos);
-					Console.WriteLine("BLOCK"); // action: WAIT or BLOCK
+					Console.Error.WriteLine("Executing {0}", nextCommand);
+					Console.WriteLine(nextCommand.Action);
+					bestPath.Dequeue();
 				}
+				//var targetPos = bestPath[cloneFloor];
+				//if (clonePos == -targetPos)
+				//{
+				//	Console.Error.WriteLine("Creating elevator at {0}", clonePos);
+				//	Console.WriteLine("ELEVATOR");
+				//	bestPath[cloneFloor] = -bestPath[cloneFloor];
+				//}
+				//else if (clonePos == targetPos)
+				//{
+				//	Console.Error.WriteLine("Reached elevator at {0}", clonePos);
+				//	Console.WriteLine("WAIT"); // action: WAIT or BLOCK
+				//}
+				//else if (hasCorrectDirection(clonePos, targetPos, direction == "RIGHT"))
+				//{
+				//	Console.Error.WriteLine("Traveling to {0}", targetPos);
+					//Console.WriteLine("WAIT"); // action: WAIT or BLOCK
+				//}
+				//else
+				//{
+				//	Console.Error.WriteLine("Blocking at {0} so clones can reach {1}", clonePos, targetPos);
+				//	Console.WriteLine("BLOCK"); // action: WAIT or BLOCK
+				//}
 			}
 		}
 	}
 
-	private static IEnumerable<int[]> pathsFrom(int fromFloor, int fromPos, int toFloor, int toPos, ILookup<int, int> elevatorsLookup, int nbAdditionalElevators)
+	private static bool hasCorrectDirection(int clonePos, int targetPos, bool travellingRight)
 	{
-		if (fromFloor == toFloor)
+		return (travellingRight) ^ (clonePos > Math.Abs(targetPos));
+	}
+
+	private static IEnumerable<Command[]> pathsFrom(int currentFloor, int currentPosition, bool headingRight, int exitFloor, int exitPosition, ILookup<int, int> elevatorsPerFloor, int additionalElevators)
+	{
+		Console.Error.WriteLine("Evaluating paths from {0}/{1}->{2}", currentFloor, currentPosition, headingRight ? "Right" : "Left");
+		if (currentFloor == exitFloor)
 		{
-			//On the right floor, so just go to exit
-			yield return new[] { toPos };
+			Console.Error.WriteLine("This is the exit floor");
+			var commands = new List<Command>();
+			if (exitPosition != currentPosition)
+			{
+				var exitIsToRight = exitPosition > currentPosition;
+				if (exitIsToRight ^ headingRight)
+					commands.Add(new Command(currentFloor, currentPosition, "BLOCK"));
+
+				commands.Add(new Command(exitFloor, exitPosition, "WAIT"));
+			}
+			yield return commands.ToArray();
 		}
 		else
 		{
 			//Try paths for any existing elevator on the floor
-			var elevatorPositions = elevatorsLookup[fromFloor];
-			foreach (var position in elevatorPositions)
+			var elevatorPositions = elevatorsPerFloor[currentFloor];
+			Console.Error.WriteLine("Found {0} elevators on floor {1}", elevatorPositions.Count(), currentFloor);
+			foreach (var elevatorPosition in elevatorPositions)
 			{
-				var paths = pathsFrom(fromFloor + 1, position, toFloor, toPos, elevatorsLookup, nbAdditionalElevators).ToArray();
+				Console.Error.WriteLine("Evaluating elevator at {0}/{1}", currentFloor, elevatorPosition);
+				var leaveFloorHeadingRight = headingRight;
+				var optionalBlockCommand = new Command[0];
+				if (currentPosition != elevatorPosition)
+				{
+					var elevatorIsToRight = elevatorPosition > currentPosition;
+					if (elevatorIsToRight ^ headingRight)
+					{
+						optionalBlockCommand = new[] { 
+							new Command(currentFloor, currentPosition, "BLOCK"), 
+						};
+						leaveFloorHeadingRight = !leaveFloorHeadingRight;
+					}
+				}
+
+				var levelCommands = optionalBlockCommand
+					.Concat(new[]{ new Command(currentFloor, elevatorPosition, "WAIT") })
+					.ToArray();
+
+				var paths = pathsFrom(currentFloor + 1, elevatorPosition, leaveFloorHeadingRight, exitFloor, exitPosition, elevatorsPerFloor, additionalElevators).ToArray();
 				foreach (var path in paths)
 				{
-					yield return new[] { position }.Concat(path).ToArray();
+					yield return levelCommands.Concat(path).ToArray();
 				}
 			}
 
 			//If there are additional elevators available, try using one of them
-			if (nbAdditionalElevators > 0)
+			if (additionalElevators > 0)
 			{
-				var paths = pathsFrom(fromFloor + 1, fromPos, toFloor, toPos, elevatorsLookup, nbAdditionalElevators - 1).ToArray();
+				Console.Error.WriteLine("Evaluating a new elevator (of {2}) at {0}/{1}", currentFloor, currentPosition, additionalElevators);
+				var levelCommands = new[] { new Command(currentFloor, currentPosition, "ELEVATOR") };
+				var paths = pathsFrom(currentFloor + 1, currentPosition, headingRight, exitFloor, exitPosition, elevatorsPerFloor, additionalElevators - 1).ToArray();
 				foreach (var path in paths)
 				{
-					yield return new[] { -fromPos }.Concat(path).ToArray();
+					yield return levelCommands.Concat(path).ToArray();
 				}
 			}
 		}
 	}
 
+}
+
+class Command
+{
+	public int Floor { get; set; }
+	public int Position { get; set; }
+	public string Action { get; set; }
+
+	public Command(int floor, int position, string action)
+	{
+		Floor = floor;
+		Position = position;
+		Action = action;
+	}
+
+	public override string ToString()
+	{
+		return string.Format("[{0}/{1} {2}]", Floor, Position, Action);
+	}
 }
