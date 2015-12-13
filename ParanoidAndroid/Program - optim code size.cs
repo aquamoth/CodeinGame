1b6af8ae2020e7a39f7e8f402420d3669666f5f1
Mattias Åslund <mattias.aslund@mantex.se>
2015-12-13 07:35:37 +0700

    Code size 564

--- ParanoidAndroid/Program - optim code size.cs
+++ ParanoidAndroid/Program - optim code size.cs
@@ -2,18 +2,16 @@
 using System.Linq;
 using System.Collections.Generic;
 class P{static void Main(){
-var a=Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
-int exitFloor=a[3]; 
-int exitPos=a[4];
-var e=new Dictionary<int, int>();
-for(int i=0;i<a[7];i++){
-var b=Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
-e.Add(b[0], b[1]);
+var c=Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+var l=new Dictionary<int, int>();
+for(int i=0;i<c[7];i++){
+var e=Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+l.Add(e[0], e[1]);
 }
-e.Add(exitFloor, exitPos);
+l.Add(c[3], c[4]);
 while (true)
 {
-var r2 = Console.ReadLine().Split(' ');
-var r = r2.Take(2).Select(x => int.Parse(x)).ToArray();
-Console.WriteLine((r[0]==-1||r[1]==e[r[0]]||(r2.Last()=="RIGHT")^(r[1]>e[r[0]]))?"WAIT":"BLOCK");
+var q = Console.ReadLine().Split(' ');
+var r = q.Take(2).Select(x => int.Parse(x)).ToArray();
+Console.WriteLine((r[0]==-1||r[1]==l[r[0]]||(q.Last()!="LEFT")^(r[1]>l[r[0]]))?"WAIT":"BLOCK");
 }}}
