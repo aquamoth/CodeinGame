b346754d42c19f37751c63e6956c83d65f755c5e
Mattias Åslund <mattias.aslund@mantex.se>
2016-04-09 07:51:40 +0200

    TextAlignment community puzzle.
    Game of Drones multi-player game.
    First stupid implementation; Just spread out evenly over all zones and position at center

---
+++ GameOfDrones/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+        int numberOfPlayers = int.Parse(inputs[0]); // number of players in the game (2 to 4 players)
+        int me = int.Parse(inputs[1]); // ID of your player (0, 1, 2, or 3)
+        int dronesPerPlayer = int.Parse(inputs[2]); // number of drones in each team (3 to 11)
+        int Z = int.Parse(inputs[3]); // number of zones on the map (4 to 8)
+
+        var zones = new Point[Z];
+        for (int i = 0; i < Z; i++)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            int X = int.Parse(inputs[0]); // corresponds to the position of the center of a zone. A zone is a circle with a radius of 100 units.
+            int Y = int.Parse(inputs[1]);
+            zones[i] = new Point(X, Y);
+        }
+
+        // game loop
+        while (true)
+        {
+            for (int i = 0; i < zones.Length; i++)
+            {
+                int TID = int.Parse(Console.ReadLine()); // ID of the team controlling the zone (0, 1, 2, or 3) or -1 if it is not controlled. The zones are given in the same order as in the initialization.
+            }
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                for (int j = 0; j < dronesPerPlayer; j++)
+                {
+                    inputs = Console.ReadLine().Split(' ');
+                    int DX = int.Parse(inputs[0]); // The first D lines contain the coordinates of drones of a player with the ID 0, the following D lines those of the drones of player 1, and thus it continues until the last player.
+                    int DY = int.Parse(inputs[1]);
+                }
+            }
+            for (int i = 0; i < dronesPerPlayer; i++)
+            {
+                Console.WriteLine(zones[i % zones.Length]);
+            }
+        }
+    }
+
+    class Point
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", X, Y);
+        }
+    }
+}
---
+++ TextAlignment/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        var alignmentString = Console.ReadLine();
+
+        int N = int.Parse(Console.ReadLine());
+
+        var lines = new string[N];
+        for (int i = 0; i < N; i++)
+            lines[i] = Console.ReadLine();
+
+        var alignment = (Alignment)Enum.Parse(typeof(Alignment), alignmentString);
+        var width = lines.Select(t => t.Length).Max();
+        var alignedLines = alignText(lines, alignment, width);
+
+        foreach (var text in alignedLines)
+            Console.WriteLine(text);
+    }
+
+    static IEnumerable<string> alignText(IEnumerable<string> lines, Alignment alignment, int width)
+    {
+        switch (alignment)
+        {
+            case Alignment.LEFT:
+                return lines;
+
+            case Alignment.RIGHT:
+                return lines.Select(t => new String(' ', width - t.Length) + t);
+
+            case Alignment.CENTER:
+                return lines.Select(t => new String(' ', (width - t.Length) / 2) + t);
+
+            case Alignment.JUSTIFY:
+                return lines.Select(t =>
+                {
+                    var words = t.Split();
+                    var spaceRequired = width - t.Length;
+                    var spacing = spaceRequired / (words.Length - 1) + 1;
+                    return string.Join(new String(' ', spacing), words);
+                });
+
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    enum Alignment
+    {
+        LEFT,
+        RIGHT,
+        CENTER,
+        JUSTIFY
+    }
+}
