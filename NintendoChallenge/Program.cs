349ba4b9ffe508db261a6b8335fbd2fb709380ed
Mattias Åslund <mattias.aslund@mantex.se>
2016-04-11 08:10:51 +0200

    Passing all single-bit tests

--- NintendoChallenge/Program.cs
+++ NintendoChallenge/Program.cs
@@ -62,17 +62,42 @@ namespace NintendoChallenge
             //writeEncode(new[] { "32", "c0000000", "80000000" });
 
 
-            //for (int i = 30; i >= 0; i--)
-            //{
-            //    writeDecode("00000000", uintToHex((uint)1 << i));
-            //}
-            //for (int i = 31; i >= 30; i--)
-            //{
-            //    writeDecode(uintToHex((uint)1 << i), "00000000");
-            //}
-            writeDecode("00000009", "00000000");
+            Console.Error.WriteLine("Testing low bits");
+            for (int i = 0; i < 32; i++)
+            {
+                testDecoder(0U, 1U << i);
+            }
+            Console.Error.WriteLine("Testing high bits");
+            for (int i = 32; i < 64; i++)
+            {
+                testDecoder(1U << i, 0U);
+            }
 
+            //writeDecode("00000009", "00000000");
+            //writeDecode("0000ae47", "00000000");
+
+            //writeDecode("00000020", "00000049");
             Console.ReadLine();
+        }
+
+        private static void testDecoder(uint lsb, uint msb)
+        {
+            int passedCounter = 0, failedCounter = 0;
+            foreach (var decodedValue in centurianDecoder(lsb, msb))
+            {
+                var result = centaurianOperation(32, ulong2uints(decodedValue));
+                if (result[0] != lsb || result[1] != msb)
+                {
+                    Console.Error.WriteLine("Expected {0} {1} decoded to {2}, which reencoded to {3} {4}", hexOf(lsb), hexOf(msb), hexOf(decodedValue), hexOf(result[0]), hexOf(result[1]));
+                    failedCounter++;
+                }
+                else
+                    passedCounter++;
+            }
+            if(failedCounter>0)
+            {
+                Console.Error.WriteLine("{0} tests passed and {1} failed", passedCounter, failedCounter);
+            }
         }
 
         static void writeDecode(params string[] args)
@@ -95,48 +120,42 @@ namespace NintendoChallenge
         static IEnumerable<ulong> centurianDecoder(uint lsb, uint msb)
         {
             var solutions = new List<ulong>(new[] { 0UL });
+            var encodedValue = uints2ulong(lsb, msb);
 
-            ////For each bit in msb
-            ////Bit 31 can never be set!
-            //for (int bit = 30; bit >= 0; bit--)
-            //{
-            //    var mask = ((uint)1 << bit);
-            //    var expectedBitValue = msb & mask;
-            //    if (expectedBitValue != 0)
-            //    {
-            //        var newSolutions = new List<uint[]>();
-            //        foreach (var solution in solutions)
-            //        {
-            //            //Find all ways to manipulate the solution to solve for this bit
-            //            newSolutions.AddRange(flipMsbBitIn(solution, bit));
-            //        }
-            //        solutions = newSolutions;
-            //    }
-            //}
+            solutions = centurianDecoder_lowbits(solutions, encodedValue);
+            solutions = centurianDecoder_highbits(solutions, encodedValue);
 
+            return solutions;
+        }
 
+        private static List<ulong> centurianDecoder_lowbits(List<ulong> solutions, ulong encodedValue)
+        {
             //For each bit
-            for (int bit = 0; bit < 64; bit++)
+            for (byte bit = 0; bit < 32; bit++)
             {
                 var mask = ((ulong)1 << bit);
-                var expectedBitValue = lsb & mask;
+                var expectedBitValue = encodedValue & mask;
                 if (expectedBitValue != 0)
                 {
                     var newSolutions = new List<ulong>();
                     foreach (var solution in solutions)
                     {
+                        var lowestSetBit = lowestSetBitOf(solution, bit);
+
                         //Find all ways to manipulate the solution to solve for this bit
-                        for (int j = 0; j <= bit; j++)
+                        for (int lowBitToSet = 0; lowBitToSet <= bit; lowBitToSet++)
                         {
-                            var mask0 = (uint)1 << j;
-                            var mask1 = (uint)1 << (bit - j);
+                            var highBitToSet = bit - lowBitToSet;
+                            if (lowBitToSet > highBitToSet)
+                                break;
 
-                            //TODO: What side-effects would this incur? Find remedies for all before accepting this as an answer
+                            var lowBitValue = 1UL << lowBitToSet;
+                            var highBitValue = 0x1UL << (32 + (highBitToSet % 32));
 
-                            var parts = ulong2uints(solution);
-                            var v0 = parts[0] | mask0;
-                            var v1 = parts[1] | mask1;
-                            newSolutions.Add(uints2ulong(v0, v1));
+                            if ((lowestSetBit + lowBitToSet < bit) && (solution & lowBitValue) == 0)
+                                continue;
+
+                            newSolutions.Add(solution | highBitValue | lowBitValue);
                         }
                     }
                     solutions = newSolutions;
@@ -146,6 +165,67 @@ namespace NintendoChallenge
             return solutions;
         }
 
+
+        private static List<ulong> centurianDecoder_highbits(List<ulong> solutions, ulong encodedValue)
+        {
+            //For each bit
+            for (byte bit = 32; bit < 64; bit++)
+            {
+                var mask = ((ulong)1 << bit);
+                var expectedBitValue = encodedValue & mask;
+                if (expectedBitValue != 0)
+                {
+                    var newSolutions = new List<ulong>();
+                    foreach (var solution in solutions)
+                    {
+                        var lowestSetBit = lowestSetBitOf(solution, bit);
+
+                        //Find all ways to manipulate the solution to solve for this bit
+                        for (int lowBitToSet = 0; lowBitToSet < 32; lowBitToSet++)
+                        {
+                            var highBitToSet = bit - lowBitToSet;
+                            if (highBitToSet >= 32)
+                                continue;
+
+                            if (lowBitToSet > highBitToSet)
+                                break;
+
+                            var lowBitValue = 1UL << lowBitToSet;
+                            var highBitValue = 0x1UL << (32 + highBitToSet);
+
+                            if ((lowestSetBit + lowBitToSet < bit) && (solution & lowBitValue) == 0)
+                                continue;
+
+
+                            //if (newSolutions.Count==15)
+                            //{
+                            //    var lowHex = hexOf(lowBitValue);
+                            //    var highHex = hexOf(highBitValue);
+                            //    var x = 0;
+                            //}
+
+                            newSolutions.Add(solution | highBitValue | lowBitValue);
+                        }
+                    }
+                    solutions = newSolutions;
+                }
+            }
+
+            return solutions;
+        }
+
+        private static byte lowestSetBitOf(ulong solution, byte highestBitToTest)
+        {
+            var bitValue = 1UL;
+            for(byte i = 0; i < highestBitToTest; i++)
+            {
+                if ((solution & bitValue) != 0)
+                    return i;
+                bitValue = bitValue << 1;
+            }
+            return highestBitToTest;
+        }
+
         static ulong uints2ulong(uint lsb, uint msb)
         {
             return (ulong)lsb | (((ulong)msb) << 32);
@@ -153,9 +233,10 @@ namespace NintendoChallenge
 
         static uint[] ulong2uints(ulong value)
         {
-            return new uint[] { (uint)(value & 0xffff), (uint)(value >> 32) };
+            return new uint[] { (uint)(value & 0xffffffff), (uint)(value >> 32) };
         }
 
+        static string hexOf(ulong value) { return string.Join(" ", uintToHex(ulong2uints(value))); }
 
         private static IEnumerable<uint[]> flipMsbBitIn(uint[] solution, int msbBit)
         {
@@ -207,7 +288,7 @@ namespace NintendoChallenge
         {
             return b.Select(x => x.ToString("x8")).ToArray();
         }
-        private static string uintToHex(uint b)
+        private static string hexOf(uint b)
         {
             return b.ToString("x8");
         }
